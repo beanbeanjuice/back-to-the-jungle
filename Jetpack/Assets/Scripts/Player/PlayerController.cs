acb8d347075b9e004fa1314796ea62ea7604b65a
beanbeanjuice <beanbeanjuice@outlook.com>
2023-05-27 05:31:46 -0700

    Added Fly Logic

--- Jetpack/Assets/Scripts/Player/PlayerController.cs
+++ Jetpack/Assets/Scripts/Player/PlayerController.cs
@@ -5,16 +5,38 @@ namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private float velocity;
+        [SerializeField] private float acceleration;
+        [SerializeField] private float jumpVelocity;
+
+        private Rigidbody2D _rb;
+
         // Start is called before the first frame update
         void Start()
         {
+            _rb = GetComponent<Rigidbody2D>();
 
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
+            if (Input.GetButton("Jump"))  // TODO: Left click as well?
+            {
+                _rb.velocity = new Vector3(0, jumpVelocity, 0);
+            }
 
+            // Cancels and Rotation
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        void LateUpdate()
+        {
+            //Vector2 currentPosition = this.transform.position;
+            //this.velocity += this.acceleration * Time.fixedDeltaTime;
+            //currentPosition.x += this.velocity * Time.deltaTime;
+
+
+            //this.transform.position += (Vector3)currentPosition;
         }
 
     }
