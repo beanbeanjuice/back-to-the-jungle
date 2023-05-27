4d2620e5afd8052589be930436ce20694f1fd184
beanbeanjuice <beanbeanjuice@outlook.com>
2023-05-27 04:52:38 -0700

    Added Layer Ordering

---
+++ Jetpack/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private GameObject player;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            Vector2 playerPosition = this.player.transform.position;
+            Vector3 cameraPosition = this.transform.position;
+
+            Vector3 newCameraPosition = new Vector3(playerPosition.x, cameraPosition.y, cameraPosition.z);
+
+            // Setting the Camera Position
+            this.transform.position = newCameraPosition;
+        }
+    }
+}
