d9abdfe973e8e3ba598a182baab9e2fdfb97d92a
beanbeanjuice <beanbeanjuice@outlook.com>
2023-05-27 06:42:31 -0700

    Removed Old Class

--- Jetpack/Assets/Scripts/Camera/CameraController.cs
+++ Jetpack/Assets/Scripts/Camera/CameraController.cs
@@ -7,18 +7,10 @@ namespace Camera
         [SerializeField] private GameObject player;
         [SerializeField] private float xOffset;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
         void LateUpdate()
         {
             Vector2 playerPosition = this.player.transform.position;
             Vector3 cameraPosition = this.transform.position;
-
             Vector3 newCameraPosition = new Vector3(playerPosition.x + this.xOffset, cameraPosition.y, cameraPosition.z);
 
             // Setting the Camera Position
