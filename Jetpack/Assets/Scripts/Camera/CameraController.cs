ebb3b2ce7b3dc03f5e8c9d2a024846ca7d3cf602
beanbeanjuice <beanbeanjuice@outlook.com>
2023-05-27 05:04:07 -0700

    Added Collisions

--- Jetpack/Assets/Scripts/Camera/CameraController.cs
+++ Jetpack/Assets/Scripts/Camera/CameraController.cs
@@ -5,6 +5,7 @@ namespace Camera
     public class CameraController : MonoBehaviour
     {
         [SerializeField] private GameObject player;
+        [SerializeField] private float xOffset;
 
         // Start is called before the first frame update
         void Start()
@@ -18,7 +19,7 @@ namespace Camera
             Vector2 playerPosition = this.player.transform.position;
             Vector3 cameraPosition = this.transform.position;
 
-            Vector3 newCameraPosition = new Vector3(playerPosition.x, cameraPosition.y, cameraPosition.z);
+            Vector3 newCameraPosition = new Vector3(playerPosition.x + this.xOffset, cameraPosition.y, cameraPosition.z);
 
             // Setting the Camera Position
             this.transform.position = newCameraPosition;
