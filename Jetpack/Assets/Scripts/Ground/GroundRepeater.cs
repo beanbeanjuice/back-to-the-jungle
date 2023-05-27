52757b6d7dc723d077531c67d4d68c0658ae9c74
beanbeanjuice <beanbeanjuice@outlook.com>
2023-05-27 06:38:42 -0700

    Completed Initial Player Movement

---
+++ Jetpack/Assets/Scripts/Ground/GroundRepeater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ground
+{
+    public class GroundRepeater : MonoBehaviour
+    {
+
+        private BoxCollider2D _boxCollider;
+        private Rigidbody2D _rb;
+        private float _width;
+        private float speed;
+
+        private void Start()
+        {
+            this._boxCollider = GetComponent<BoxCollider2D>();
+            this._rb = GetComponent<Rigidbody2D>();
+            this._width = this._boxCollider.size.x;
+        }
+
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
