09856b8e1096cbe4c9334a144afc095a20d0af1c
beanbeanjuice <beanbeanjuice@outlook.com>
2023-05-31 10:29:13 -0700

    Added Looping Ground

--- Jetpack/Assets/Scripts/LoopingGround.cs
+++ Jetpack/Assets/Scripts/LoopingGround.cs
@@ -5,37 +5,31 @@ using UnityEngine;
 public class LoopingGround : MonoBehaviour
 {
     public GameObject groundPrefab;
-    private Transform groundTransform;
-    private Transform playerTransform;
-    private float groundLength = 94.53f;
-    private int groundTilesOnScreen = 1;
-    private float spawnX = 94.53f;
+    public float spawnDistance = 94.53f;
+    public GameObject player;
+
+    private float lastSpawnPosition;
 
     private void Start()
     {
-        this.groundTransform = GameObject.FindGameObjectWithTag("GroundManager").transform;
-        this.playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        SpawnGround();
-        
+        this.lastSpawnPosition = groundPrefab.transform.position.x;
     }
 
     private void Update()
     {
-        if (playerTransform.position.x > (this.spawnX - groundTilesOnScreen * groundLength))
+        float playerPosition = player.transform.position.x;
+
+        if (playerPosition > (this.lastSpawnPosition + this.spawnDistance) / 2)
         {
-            // Debug.Log(playerTransform.position.x + " > (" + spawnX + " - " + groundTilesOnScreen + " * " + groundLength + ")");
-            Debug.Log("During Update, spawnX = " + this.spawnX);
             SpawnGround();
+            lastSpawnPosition += spawnDistance;
         }
     }
 
     private void SpawnGround()
     {
-        GameObject ground;
-        ground = Instantiate(this.groundPrefab) as GameObject;
-        ground.transform.SetParent(this.groundTransform);
-        ground.transform.position = new Vector2(this.spawnX,this.groundPrefab.transform.position.y);
-        this.spawnX += this.groundLength;
-        Debug.Log("After addition of groundLength, spawnX = " + this.spawnX);
+        GameObject newGround = Instantiate(groundPrefab, transform);
+        float yPos = groundPrefab.transform.position.y;
+        newGround.transform.position = new Vector2(lastSpawnPosition + spawnDistance, yPos);
     }
 }
