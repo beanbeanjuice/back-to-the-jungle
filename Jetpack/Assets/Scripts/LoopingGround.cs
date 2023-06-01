7329fd559586ae78f21c1badd90eda2b731237cf
gpetty002 <56804160+gpetty002@users.noreply.github.com>
2023-05-31 22:39:03 -0700

    FEATURE - Infinite Ground - Fixed logic in LoopingGround.cs to handle ground properties - Added LoopingGround.cs script to Ground prefab

--- Jetpack/Assets/Scripts/LoopingGround.cs
+++ Jetpack/Assets/Scripts/LoopingGround.cs
@@ -4,32 +4,54 @@ using UnityEngine;
 
 public class LoopingGround : MonoBehaviour
 {
-    public GameObject groundPrefab;
-    public float spawnDistance = 94.53f;
-    public GameObject player;
+    [SerializeField] private GameObject groundPrefab;
+    [SerializeField] private GameObject grid;
+    [SerializeField] private float spawnDistance; // ground length
+    [SerializeField] private GameObject player;
+    [SerializeField] private float secondsBeforeDelete;
+
+    // private float spawnY = -3.0f; // 2.368249
 
     private float lastSpawnPosition;
 
     private void Start()
     {
-        this.lastSpawnPosition = groundPrefab.transform.position.x;
+        this.lastSpawnPosition = groundPrefab.transform.position.x; // -15.7, -9.89
+
     }
 
     private void Update()
     {
         float playerPosition = player.transform.position.x;
-
+        // lastSpawnPosition = -15.7
+        // playerPosition = -9.89
+        // spawnDistance = 40
+        // 36
         if (playerPosition > (this.lastSpawnPosition + this.spawnDistance) / 2)
         {
             SpawnGround();
             lastSpawnPosition += spawnDistance;
+
         }
+        // Destroy(this.gameObject, this.secondsBeforeDelete);
+
     }
 
     private void SpawnGround()
     {
-        GameObject newGround = Instantiate(groundPrefab, transform);
-        float yPos = groundPrefab.transform.position.y;
-        newGround.transform.position = new Vector2(lastSpawnPosition + spawnDistance, yPos);
+        // Transform groundPos = new Vector2(lastSpawnPosition + spawnDistance, this.spawnY);
+        // Transform groundTransform;
+        // groundTransform.position = groundPos;
+        // GameObject newGround = Instantiate(groundPrefab, new Vector3(lastSpawnPosition + spawnDistance, 0, 0), Quaternion.identity);
+        // Transform myGround = this.groundPrefab.transform;
+        // myGround.transform.position = new Vector3(lastSpawnPosition + spawnDistance, 0, 0);
+        // newGround.transform.SetParent(myGround);
+        GameObject newGround = Instantiate(groundPrefab, this.groundPrefab.transform);
+        newGround.transform.SetParent(this.grid.transform);
+        newGround.transform.localScale = new Vector3(0.4488856f, 0.6523f, 0);
+        newGround.transform.position = new Vector3(lastSpawnPosition + spawnDistance, -2.73f, 60.0f);
+
+        //0.4488856
+        //0.6523
     }
 }
