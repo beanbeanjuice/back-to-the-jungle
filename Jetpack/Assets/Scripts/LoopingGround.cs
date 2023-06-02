619d98684a689b2b3b92f504c07b087db3194b6d
gpetty002 <56804160+gpetty002@users.noreply.github.com>
2023-06-01 17:47:38 -0700

    Fixed linting errors

--- Jetpack/Assets/Scripts/LoopingGround.cs
+++ Jetpack/Assets/Scripts/LoopingGround.cs
@@ -1,10 +1,8 @@
 using UnityEngine;
 
     /// <summary>
-    /// Looping Ground. This class is used for generating
-    /// and spawning ground in.
+    /// Looping Ground. This class is used for generating and spawning ground in.
     /// </summary>
-
 public class LoopingGround : MonoBehaviour
 {
     [SerializeField] public GameObject currentGround;
@@ -14,7 +12,6 @@ public class LoopingGround : MonoBehaviour
     [SerializeField] private GameObject player;
     private float _endOfGround;
     private bool _onCurrent = true;
-
     private void Start()
     {
         this._endOfGround = this.currentGround.transform.position.x + this.spawnDistance;
@@ -30,8 +27,10 @@ public class LoopingGround : MonoBehaviour
     }
     private void SwitchGround()
     {
+        // Switch between grounds here. If we are on the GameObject currentGround prepare the GameObject nextGround to move forward
         if (this._onCurrent)
         {
+            // Dimensions used here are from sprites used in game.
             this._endOfGround = this.nextGround.transform.position.x + this.spawnDistance;
             this.currentGround.transform.position = new Vector3(this._endOfGround, -2.73f, 60.0f);
             this._onCurrent = false;
