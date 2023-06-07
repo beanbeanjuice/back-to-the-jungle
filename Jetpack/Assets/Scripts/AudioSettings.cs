b2a5b6aa8bc4aa57bae387ecb399059616d72bce
nwestl <wesnguyen@ucdavis.edu>
2023-06-06 18:41:27 -0700

    Mixer and AudioSettings
    -Added audio mixers: Master, SFX, Music.
    -Implemented AudioSettings.cs to adjust the mixers ingame.

---
+++ Jetpack/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+/// <summary>
+/// Allows player to change audio mixer in settings menu.
+/// <remarks>Coded by Westley.</remarks>
+/// </summary>
+public class AudioSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+
+    public void SetMasterVolume(float volume)
+    {
+        mixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        mixer.SetFloat("SFXVolume", volume);
+    }
+    
+    public void SetMusicVolume(float volume)
+    {
+        mixer.SetFloat("MusicVolume", volume);
+    }
+}
