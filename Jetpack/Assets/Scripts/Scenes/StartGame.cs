817175c35101986750670c9a18bb532b8ad62976
roxanneruan <roxanneruan@gmail.com>
2023-06-09 00:43:43 -0700

    transition to Gameplay Scene

--- Jetpack/Assets/Scripts/Scenes/StartGame.cs
+++ Jetpack/Assets/Scripts/Scenes/StartGame.cs
@@ -1,18 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartGame : MonoBehaviour
+namespace Scenes
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// A class to transition out of lab cutscene into gameplay scene.
+    /// <remarks> Referenced from:
+    /// https://www.youtube.com/watch?v=0soGRKtpJuA&ab_channel=TheGameDevShow</remarks>
+    /// </summary>
+    public class StartGame : MonoBehaviour
     {
-        
-    }
+        void Start()
+        {
+            StartCoroutine(NextScene());
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Update()
+        {
+
+        }
+
+        private IEnumerator NextScene()
+        {
+            yield return new WaitForSeconds(2.45f);
+            SceneManager.LoadScene("Gameplay");
+        }
     }
 }
