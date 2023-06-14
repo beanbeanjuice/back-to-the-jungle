2f35d35e493827ac4c35348d286bf009f7560240
nwestl <wesnguyen@ucdavis.edu>
2023-06-14 02:16:04 -0700

    Update Score.cs

--- Jetpack/Assets/Scripts/Player/Score.cs
+++ Jetpack/Assets/Scripts/Player/Score.cs
@@ -1,29 +1,25 @@
 using UnityEngine;
 using TMPro;
 
-namespace Player
+/// <summary>
+/// A script used to display the distance traveled and the amount of fish collected.
+/// <remarks>Coded by Westley</remarks>
+/// </summary>
+public class Score : MonoBehaviour
 {
-    /// <summary>
-    /// A script used to display the distance traveled and the amount of fish collected.
-    /// <remarks>Coded by Westley</remarks>
-    /// </summary>
-    public class Score : MonoBehaviour
+    [SerializeField] private TextMeshProUGUI distanceScore;
+    [SerializeField] private TextMeshProUGUI fishScore;
+    private PlayerController _playerStats;
+
+    private void Start()
     {
-        [SerializeField] private TextMeshProUGUI distanceScore;
-        [SerializeField] private TextMeshProUGUI fishScore;
-
-        private PlayerController _playerStats;
-
-        private void Start()
-        {
-            this._playerStats = this.gameObject.GetComponent<PlayerController>();
-        }
-
-        private void Update()
-        {
-            // Updates the UI with distance traveled and fish collected scores.
-            this.distanceScore.text = this._playerStats.GetDistanceRun().ToString("0");
-            this.fishScore.text = this._playerStats.GetScore().ToString("0");
-        }
+        this._playerStats = this.gameObject.GetComponent<PlayerController>();
     }
+
+    private void Update()
+    {
+        // Updates the UI with distance traveled and fish collected scores.
+        this.distanceScore.text = this._playerStats.GetDistanceRun().ToString("0");
+        this.fishScore.text = this._playerStats.GetScore().ToString("0");
+    }    
 }
